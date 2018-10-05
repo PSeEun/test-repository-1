a66ca6b5d2e1561581b16ad31166966200c8fad7
C:\Users\DS\Documents\test-repository <wlsdlftpdms@naver.com>
2018-10-05 14:44:13 +0900

    Multiple 함수 추가 - 세영언니꺼

--- Calculator.java
+++ Calculator.java
@@ -6,4 +6,9 @@ public class Calculator{
     public static int substract(int a, int b){
         return a-b;
     }
+
+    //수정
+    public static int multiple(int a,int b){
+        return a*b;
+    }
 }
